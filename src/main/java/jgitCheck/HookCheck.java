072397408f1cfcea8d742a26c38e17cadf2e4b17
unknown <E942PX@XD-DW764-289.na.erac.com>
2015-07-03 15:37:50 +0530

    ff

---
+++ src/main/java/jgitCheck/HookCheck.java
@@ -0,0 +1,16 @@
+package jgitCheck;
+
+import java.util.Collection;
+
+import org.eclipse.jgit.transport.PostReceiveHook;
+import org.eclipse.jgit.transport.ReceiveCommand;
+import org.eclipse.jgit.transport.ReceivePack;
+
+public class HookCheck implements PostReceiveHook{
+
+    public void onPostReceive(ReceivePack arg0, Collection<ReceiveCommand> arg1) {
+        
+        
+    }
+
+}
