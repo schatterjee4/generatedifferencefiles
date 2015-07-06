6f402c9411c5cda726df7625c126eaa07948d1aa
schatterjee4 <sharmistha.chatterjee@cognizant.com>
2015-07-06 17:24:15 +0530

    help

---
+++ src/main/java/jgitCheck/MainClass.java
@@ -0,0 +1,29 @@
+package jgitCheck;
+
+
+import java.io.IOException;
+
+import org.apache.wicket.util.file.File;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Repository;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+       
+            File gitWorkDir = new File("C:/temp/gittest/");
+            Git git;
+            try {
+                git = Git.open(gitWorkDir);
+                Repository repo = git.getRepository();
+                System.out.println(repo);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+       
+
+    }
+
+}
